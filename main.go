0bbc95e3c058f3d8a7a56c709755cd7e0975a5dc
Navdeep-singh-ghotra <Navdeepsingh.ghotra@gmail.com>
2025-08-03 00:33:13 -0400

    proto

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
-	pb "github.com/navdeep-singh-ghotra/kubecombe" // Your Protobuf definitionS
+	"github.com/navdeep-singh-ghotra/kubecombe/pb"
+	//pb "github.com/navdeep-singh-ghotra/kubecombe/pb" // Your Protobuf definitionS
 )
 
 type Product struct {
